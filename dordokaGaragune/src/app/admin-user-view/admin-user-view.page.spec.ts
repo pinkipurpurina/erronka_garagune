d06724d8aea167a97229aa9638fb07e1b8a40b84
ProgrammerLutherion <XD9002@gmail.com>
2021-01-14 09:02:44 +0100

    12/01/2021Erik_Gallego3

--- dordokaGaragune/src/app/admin-user-view/admin-user-view.page.spec.ts
+++ dordokaGaragune/src/app/admin-user-view/admin-user-view.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AdminUserViewPage } from './admin-user-view.page';
@@ -7,7 +7,7 @@ describe('AdminUserViewPage', () => {
   let component: AdminUserViewPage;
   let fixture: ComponentFixture<AdminUserViewPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AdminUserViewPage ],
       imports: [IonicModule.forRoot()]
