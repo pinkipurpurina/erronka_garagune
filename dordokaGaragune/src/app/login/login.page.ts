973a0e66715c5e5715d9138c0d67c53f650ffb02
Laura <fernandez.laura@uni.eus>
2021-01-20 09:30:51 +0100

    20/01/2021- laura user probak

--- dordokaGaragune/src/app/login/login.page.ts
+++ dordokaGaragune/src/app/login/login.page.ts
@@ -1,31 +1,38 @@
-import { Component, NgModule, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { Component, NgModule, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { ToastController } from '@ionic/angular';
-
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
-
-
 export class LoginPage implements OnInit {
-
   ionicForm: FormGroup;
   isSubmitted = false;
 
-  constructor(public formBuilder: FormBuilder, private authSvc: AuthService, private router: Router, public toastController: ToastController) { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private authSvc: AuthService,
+    private router: Router,
+    public toastController: ToastController
+  ) {}
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
+        ],
+      ],
       // 8 letras, una minuscula, una mayuscula, un numero y un caracter especial
       // password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}')]]
-      password: ['', [Validators.required]]
-    })
+      password: ["", [Validators.required]],
+    });
   }
 
   get errorControl() {
@@ -44,13 +51,16 @@ export class LoginPage implements OnInit {
             snap.forEach((element) => {
               var uid = element.key;
               if (user.uid == uid) {
-                this.router.navigate(['admin-user-view']);
+                console.log("user monitorea");
+                this.router.navigate(["admin-user-view"]);
+                //return user;
               }
               this.authSvc.getMonitorUsers(uid).once("value", (snap) => {
                 snap.forEach((element2) => {
                   var monitorUid = element2.key;
                   if (user.uid == monitorUid) {
-                    this.router.navigate(['user-kategoria']);
+                    console.log("user normala");
+                   // this.router.navigate(["user-kategoria"]);
                   }
                 });
               });
@@ -58,14 +68,14 @@ export class LoginPage implements OnInit {
           });
         } else {
           const toast = await this.toastController.create({
-            message: 'Email o contraseña incorrecta.',
-            duration: 2000
+            message: "Email o contraseña incorrecta.",
+            duration: 2000,
           });
           toast.present();
         }
       }
     } catch (error) {
-      console.log('Error->', error);
+      console.log("Error->", error);
     }
   }
 
@@ -73,16 +83,16 @@ export class LoginPage implements OnInit {
     try {
       const user = await this.authSvc.loginGoogle();
       if (user) {
-        this.router.navigate(['admin-user-view']);
+        this.router.navigate(["admin-user-view"]);
       } else {
         const toast = await this.toastController.create({
-          message: 'Email o contraseña incorrecta.',
-          duration: 2000
+          message: "Email o contraseña incorrecta.",
+          duration: 2000,
         });
         toast.present();
       }
     } catch (error) {
-      console.log('Error->', error);
+      console.log("Error->", error);
     }
   }
 
