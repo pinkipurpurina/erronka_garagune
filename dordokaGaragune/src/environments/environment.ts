707db582ee76748310c2e0d6265e956a30f8965a
Laura <fernandez.laura@uni.eus>
2021-01-27 09:54:46 +0100

    27/07/2021laura ultimo

--- dordokaGaragune/src/environments/environment.ts
+++ dordokaGaragune/src/environments/environment.ts
@@ -2,19 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
-    authDomain: "dordoka-37245.firebaseapp.com",
-    databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "dordoka-37245",
-    storageBucket: "dordoka-37245.appspot.com",
-    messagingSenderId: "412908482658",
-    appId: "1:412908482658:web:6532b6585c9ec29548213b",
-    measurementId: "G-74L7NE11KY"
-  }
-};
+// export const environment = {
+//   production: false,
+//   firebase:{
+//     apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
+//     authDomain: "dordoka-37245.firebaseapp.com",
+//     databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "dordoka-37245",
+//     storageBucket: "dordoka-37245.appspot.com",
+//     messagingSenderId: "412908482658",
+//     appId: "1:412908482658:web:6532b6585c9ec29548213b",
+//     measurementId: "G-74L7NE11KY"
+//   }
+// };
 
 // export const environment = {
 //   production: false,
@@ -42,7 +42,7 @@ export const environment = {
 //     appId: "1:113309518985:web:96811b8b02071265700a58"
 //   },
 // };
-export const environment = {
+export const environment = {//laura
   production: false,
   firebase: {
     apiKey: "AIzaSyC-BEcphfe-7pNpAyjnlmMMtQLUO-JhifI",
