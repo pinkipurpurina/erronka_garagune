b47c28a4538a110b823eccdfa03a1521cc7b5a31
Laura <fernandez.laura@uni.eus>
2021-02-02 14:02:18 +0100

    02/02/2021-laura- aldaketa txikiak

--- dordokaGaragune/src/environments/environment.ts
+++ dordokaGaragune/src/environments/environment.ts
@@ -2,19 +2,19 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase:{
-    apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
-    authDomain: "dordoka-37245.firebaseapp.com",
-    databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "dordoka-37245",
-    storageBucket: "dordoka-37245.appspot.com",
-    messagingSenderId: "412908482658",
-    appId: "1:412908482658:web:6532b6585c9ec29548213b",
-    measurementId: "G-74L7NE11KY"
-  }
-};
+// export const environment = {
+//   production: false,
+//   firebase:{
+//     apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
+//     authDomain: "dordoka-37245.firebaseapp.com",
+//     databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "dordoka-37245",
+//     storageBucket: "dordoka-37245.appspot.com",
+//     messagingSenderId: "412908482658",
+//     appId: "1:412908482658:web:6532b6585c9ec29548213b",
+//     measurementId: "G-74L7NE11KY"
+//   }
+// };
 
 // export const environment = {
 //   production: false,
@@ -52,31 +52,19 @@ export const environment = {
 //     appId: "1:113309518985:web:96811b8b02071265700a58"
 //   },
 // };
-// export const environment = {
-//   production: false,
-//   firebase: {
-//     apiKey: "AIzaSyC-BEcphfe-7pNpAyjnlmMMtQLUO-JhifI",
-//     authDomain: "garagune2.firebaseapp.com",
-//     databaseURL: "https://garagune2-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "garagune2",
-//     storageBucket: "garagune2.appspot.com",
-//     messagingSenderId: "717841716777",
-//     appId: "1:717841716777:web:e40992abce323e10fe2e38",
-//     measurementId: "G-V5KTJYH4NR"
-//   },
-// };
-// export const environment = {
-//   production: false,
-//   firebase : {
-//     apiKey: "AIzaSyDCInIpBx_3NwzKP5zp249aSuJYD8MCXT8",
-//     authDomain: "pruebaionic-94717.firebaseapp.com",
-//     databaseURL: "https://pruebaionic-94717-default-rtdb.firebaseio.com",
-//     projectId: "pruebaionic-94717",
-//     storageBucket: "pruebaionic-94717.appspot.com",
-//     messagingSenderId: "113309518985",
-//     appId: "1:113309518985:web:96811b8b02071265700a58"
-//   },
-// };
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC-BEcphfe-7pNpAyjnlmMMtQLUO-JhifI",
+    authDomain: "garagune2.firebaseapp.com",
+    databaseURL: "https://garagune2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "garagune2",
+    storageBucket: "garagune2.appspot.com",
+    messagingSenderId: "717841716777",
+    appId: "1:717841716777:web:e40992abce323e10fe2e38",
+    measurementId: "G-V5KTJYH4NR"
+  },
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
