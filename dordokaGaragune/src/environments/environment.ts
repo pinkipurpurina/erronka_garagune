ad2f637f0da6f1aaebc7b220c786263d5e1d8ae2
ProgrammerLutherion <XD9002@gmail.com>
2021-02-05 11:13:34 +0100

    2021/02/05 Enviroments final

--- dordokaGaragune/src/environments/environment.ts
+++ dordokaGaragune/src/environments/environment.ts
@@ -1,68 +1,17 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-// export const environment = {
-//   production: false,
-//   firebase:{
-//     apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
-//     authDomain: "dordoka-37245.firebaseapp.com",
-//     databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "dordoka-37245",
-//     storageBucket: "dordoka-37245.appspot.com",
-//     messagingSenderId: "412908482658",
-//     appId: "1:412908482658:web:6532b6585c9ec29548213b",
-//     measurementId: "G-74L7NE11KY"
-//   }
-// };
-
-// export const environment = {
-//   production: false,
-//   firebase:{
-//     apiKey: "AIzaSyBTwxhPuE6VjTo283pN0h-jrWBkxm_mgRY",
-//     authDomain: "erronkagaragune.firebaseapp.com",
-//     databaseURL:
-//       "https://erronkagaragune-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "erronkagaragune",
-//     storageBucket: "erronkagaragune.appspot.com",
-//     messagingSenderId: "969301020862",
-//     appId: "1:969301020862:web:7426444bbc7f37555a4eef",
-//     measurementId: "G-DFPV5DWPN5",
-//     apiKey: "AIzaSyAA8kKFiwMG2HZRgMqAO_KuPcNVSYz6b28",
-//     authDomain: "dordoka-37245.firebaseapp.com",
-//     databaseURL: "https://dordoka-37245-default-rtdb.europe-west1.firebasedatabase.app",
-//     projectId: "dordoka-37245",
-//     storageBucket: "dordoka-37245.appspot.com",
-//     messagingSenderId: "412908482658",
-//     appId: "1:412908482658:web:6532b6585c9ec29548213b",
-//     measurementId: "G-74L7NE11KY"
-//   }
-// };
-
-
-// export const environment = {//Be�at
-//   production: false,
-//   firebase : {
-//     apiKey: "AIzaSyDCInIpBx_3NwzKP5zp249aSuJYD8MCXT8",
-//     authDomain: "pruebaionic-94717.firebaseapp.com",
-//     databaseURL: "https://pruebaionic-94717-default-rtdb.firebaseio.com",
-//     projectId: "pruebaionic-94717",
-//     storageBucket: "pruebaionic-94717.appspot.com",
-//     messagingSenderId: "113309518985",
-//     appId: "1:113309518985:web:96811b8b02071265700a58"
-//   },
-// };
-export const environment = {
+const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC-BEcphfe-7pNpAyjnlmMMtQLUO-JhifI",
-    authDomain: "garagune2.firebaseapp.com",
-    databaseURL: "https://garagune2-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "garagune2",
-    storageBucket: "garagune2.appspot.com",
-    messagingSenderId: "717841716777",
-    appId: "1:717841716777:web:e40992abce323e10fe2e38",
-    measurementId: "G-V5KTJYH4NR"
+      apiKey: "AIzaSyBDymkjDd7REE3YGnBJ2IHQjPgz56YSnvY",
+      authDomain: "dordokagaragune.firebaseapp.com",
+      databaseURL: "https://dordokagaragune-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "dordokagaragune",
+      storageBucket: "dordokagaragune.appspot.com",
+      messagingSenderId: "934591318037",
+      appId: "1:934591318037:web:57089c3d84022c46c3261c",
+      measurementId: "G-1HKGYZT0CZ"   
   },
 };
 
