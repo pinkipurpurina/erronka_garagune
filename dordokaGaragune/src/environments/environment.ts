84cc0188c140ca9217a689b8f996beca3011f79d
ProgrammerLutherion <XD9002@gmail.com>
2021-02-05 11:19:45 +0100

    2021/02/05 Final enviroments

--- dordokaGaragune/src/environments/environment.ts
+++ dordokaGaragune/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const environment = {
+export const environment = {
   production: false,
   firebase: {
       apiKey: "AIzaSyBDymkjDd7REE3YGnBJ2IHQjPgz56YSnvY",
